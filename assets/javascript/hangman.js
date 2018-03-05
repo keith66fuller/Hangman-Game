ef639319dbe75437c5880e957d45d2108dcf62bd
Keith Fuller <keith66fuller@gmail.com>
2018-03-05 12:40:26 -0600

    done

--- assets/javascript/hangman.js
+++ assets/javascript/hangman.js
@@ -1,12 +1,19 @@
 $(document).ready(function () {
     var tries;
-    var wins;
+    var wins = 0;
     var answer;
     var guesses;
     var lettersGuessed;
     var myWord;
-    var reset;
+    var reset = 0;
     var arrLtrsGuessed;
+    var maxTries;
+    var randomMiscImgCtr = 0;
+    var randomMiscSndCtr = 0;
+    var arrMiscImg = [];
+    var miscImgCnt = 13;
+    var arrMiscSnd = [];
+    var miscSndCnt = 13;
 
     var wordBank = [
         'power',
@@ -99,38 +106,25 @@ $(document).ready(function () {
         'significance',
     ]
     ;
-    function initGame() {
-        tries = 0;
-        wins = 0;
-        answer = "";
-        guesses = 0;    
-        lettersGuessed = "";
-        myWord = wordBank[Math.floor((Math.random() * wordBank.length) + 0)];
-        reset = 0;
-        arrLtrsGuessed = [];
-        for (var i =1; i<=myWord.length; i++) {
-            answer += "_";
-        }
-        
-        maxTries = myWord.length * 3;
-    }
     function blink_text(selector) {
         $(selector).fadeOut(500);
         $(selector).fadeIn(500);
     }
+
+    function randomizeImages() {
+        for (var i = 0; i < miscImgCnt ; i++) {
+            arrMiscImg.push(i);
+        }
+        arrMiscImg.sort(function(a, b){return 0.5 - Math.random()});
+        console.log(arrMiscImg);
+    }
     
-    initGame();
-
-
-    var varFields = {
-        "hmLabelWins" : null,
-        "hmWins" : wins,
-        "hmLabelAnswer" : null,
-        "hmAnswer" : answer,
-        "hmLabelGuessesLeft" : null,
-        "hmGuessesLeft" : maxTries,
-        "hmLabelLettersGuessed" : null,
-        "hmLettersGuessed" : lettersGuessed,
+    function randomizeSounds() {
+        for (var i = 0; i < miscSndCnt ; i++) {
+            arrMiscSnd.push(i);
+        }
+        arrMiscSnd.sort(function(a, b){return 0.5 - Math.random()});
+        console.log(arrMiscSnd);
     }
 
     $('#hmLabelWins',
@@ -140,49 +134,78 @@ $(document).ready(function () {
     ).show();
 
     function labelsDisplay(toggle) {
-        if (toggle) {
-            $('#hmLabelWins',
-            '#hmLabelAnswer',
-            '#hmLabelGuessesLeft',
-            '#hmLabelLettersGuessed',
-            ).show();
-        } else {
-            $('#hmLabelWins',
-            '#hmLabelAnswer',
-            '#hmLabelGuessesLeft',
-            '#hmLabelLettersGuessed',
-            ).hide();
-        }
+        toggleDisplay('Label',toggle);
     }
 
-    function showValues() {
-        for (var element in varFields) {        
-            // console.log("ELEMENT " + element + " --> " + varFields[element]);
-            if (varFields[element] != null) {
-                document.getElementById(element).innerText = varFields[element];
-            }
-            // console.log("NEW ELEMENT " + typeof(varFields[element]));
-        }
+    function valuesDisplay(toggle) {
+        toggleDisplay('Value',toggle);
     }
 
+    function toggleDisplay(type,toggle) {
+        $('[id*=' + type + ']').css('color',toggle?'white':'black');
+    }
 
+    function updateValues() {
+        $('#hmValueWins').text(wins);
+        $('#hmValueAnswer').text(answer);
+        $('#hmValueGuessesLeft').text(maxTries-tries);
+        $('#hmValueLettersGuessed').text(lettersGuessed);
+    }
 
+    function newMiscImg() {
+        randomMiscImgCtr = (++randomMiscImgCtr==miscImgCnt)?0:randomMiscImgCtr;
+        $('#miscImage').attr('src','./assets/images/randomMisc/image_' + randomMiscImgCtr + '.gif');
+    }
 
+    function newMiscSnd() {
+        randomMiscSndCtr = (++randomMiscSndCtr==miscSndCnt)?0:randomMiscSndCtr;
+        var audio = new Audio('./assets/audio/sound_' + randomMiscSndCtr + '.wav');
+        audio.play();
+    }
+
+    function blinkText(id) {
+        $(id).fadeOut(500);
+        $(id).fadeIn(500);
+
+    }
 
     window.onload = function() {
         labelsDisplay(false);
-
+        valuesDisplay(false);
+        randomizeImages();
+        randomizeSounds();
+    
         document.onkeypress = function(event) {
+
             if (!reset) {
-                initGame();
-                $('#startMsg').css('color','black');
-                showValues();
+                // Only the ENTER key can begin the game
+                if (event.keyCode != 13) {
+                    blinkText('startMsg');
+                    return;
+                };
+                tries = 0;
+                guesses = 0;    
+                arrLtrsGuessed = [];
+                lettersGuessed = "";
+                answer = "";
+
+                myWord = wordBank[Math.floor((Math.random() * wordBank.length) + 0)];
+                // myWord = 'elephant';
+
+                for (var i =1; i<=myWord.length; i++) {
+                    answer += "_";
+                }
+                maxTries = myWord.length * 3;
+        
+                $('#startMsg').text('To guess a letter, press its key.');
+                updateValues();
                 labelsDisplay(true);
+                valuesDisplay(true);
                 reset++;
             } else {
                 if (1) { // Check for matched characters and update answer
-                    // Clear the message area
-                    $('#startMsg').css('color','black');
+
+                    $('#startMsg').text('To guess a letter, press its key.');
 
                     var key = event.key.toLowerCase();
                     // make sure an alphabetic key was pressed
@@ -192,42 +215,54 @@ $(document).ready(function () {
 
                         // bump tries
                         tries++;
-                        $('#hmGuessesLeft').text(maxTries - tries);
+                        $('#hmValueGuessesLeft').text(maxTries - tries);
 
                         // record the letter guessed
                         if (arrLtrsGuessed.indexOf(key) == -1) {
                             arrLtrsGuessed.push(key);
-                            $('#hmLettersGuessed').text(arrLtrsGuessed.join(""));
+                            $('#hmValueLettersGuessed').text(arrLtrsGuessed.join(""));
                         }
 
                         var arrAnswer = answer.split("");
-                        console.log("ANSWER ARRAY " + arrAnswer);
+                        var correct = 0;
                         for (var i = 0; i < myWord.length; i++) {
                             if (myWord[i] == key) {
                                 arrAnswer[i]=key;
+                                correct++;
                             }
                         }
                         answer = arrAnswer.join("");
-                        $('#hmAnswer').text(answer);
+                        $('#hmValueAnswer').text(answer);
+                        if (correct) {
+                            newMiscImg();
+                            //newMiscSnd();
+                            console.log("correct letter");
+                        }
+
+
                     } else {
                         $('#startMsg').text('ALPHABETIC KEYS ONLY');
                         $('#startMsg').css('color','red');
+                        blinkText('#startMsg');
+                        $('#startMsg').css('color','white');
+                        $('#startMsg').text('To guess a letter, press its key.');
                     }
                 }
                 console.log("tries " + tries);
                 if (1) { // Check win lose
                     if (answer == myWord) { // Did user guess the word?
-                        $('#startMsg').text("YOU WIN !!!!");
+                        $('#startMsg').text("YOU WIN !!!!  PRESS ENTER FOR ANOTHER GAME");
                         $('#startMsg').css('color','white');
-                        setInterval(blink_text('#startMsg'), 1000);
+                        wins++;
+                        $('#hmValueWins').text(wins);
                         reset = 0;
                     } else if (tries==maxTries) { // Is users out of tries?
-                        $('#startMsg').text("YOU LOST!!!   The word was " + myWord);
-                        $('#startMsg').css('color','red');
-                        setInterval(blink_text('#startMsg'), 1000);
+                        $('#startMsg').text("YOU LOST!!!   The word was " + myWord + ".    PRESS ENTER FOR ANOTHER GAME");
                         reset = 0;
+                        var audio = new Audio('./assets/audio/game_over_man.wav');
+                        audio.play();
                     }
-
+                
                 }
             }
         }
